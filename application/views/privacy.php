f5d96e3e5662f6ec3091d3c24551a799aff8425e
Apeliemma <livingstoneapeli@gmail.com>
2025-07-29 16:51:03 +0300

    updated

--- application/views/privacy.php
+++ application/views/privacy.php
@@ -344,7 +344,7 @@ ol li::before {
 <body>
     <div class="container">
         <div class="header">
-            <img src="<?php echo base_url() ?>assets/img/home-header-stepakash.png" alt="Stepakash" style="max-width: 220px; margin-bottom: 10px;">
+            <img src="<?php echo base_url() ?>assets/img/home-header-stepakash.png" alt="Stepakash" style="max-width: 220px; margin-bottom: 10px; filter: brightness(0) invert(1);">
             <p class="subtitle">Privacy Policy</p>
         </div>
         
